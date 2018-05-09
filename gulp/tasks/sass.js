1a146bd49fbc8bf42403f3a68fa1148192c75dd7
Sokol V <1470715@gmail.com>
2018-05-09 20:24:26 +0200

    header finish

--- gulp/tasks/sass.js
+++ gulp/tasks/sass.js
@@ -7,10 +7,10 @@ module.exports = function() {
             .pipe($.gp.sassGlob())
             .pipe($.gp.sass()).on('error', $.gp.notify.onError({ title: 'Style' }))
             .pipe($.gp.autoprefixer({ browsers: $.config.autoprefixerConfig }))
-            .pipe($.gp.cssUnit({
+/*            .pipe($.gp.cssUnit({
             	type: 'px-to-rem',
             	rootSize: 16
-            }))
+            }))*/
             .pipe($.gp.csso({debug: true}))
             .pipe($.gp.sourcemaps.write())
             .pipe($.gulp.dest($.config.build.css))
